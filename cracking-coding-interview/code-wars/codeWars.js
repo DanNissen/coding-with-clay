bd07bf9125fad5d6dba5a6620191e703100dd2ee
Kleowulf <claytond921@gmail.com>
2024-08-14 19:08:52 -0600

    more code wars tests

--- cracking-coding-interview/code-wars/codeWars.js
+++ cracking-coding-interview/code-wars/codeWars.js
@@ -1,114 +1,124 @@
 export const doubleCharacters = (givenString) => {
-    const givenStringCharactersArray = givenString.split('')
-    let solutionArray = []
-    givenStringCharactersArray.forEach(character => {
-        solutionArray.push(character)
-        solutionArray.push(character)
-    });
-    return solutionArray.join("")
+  const givenStringCharactersArray = givenString.split('')
+  let solutionArray = []
+  givenStringCharactersArray.forEach(character => {
+    solutionArray.push(character)
+    solutionArray.push(character)
+  });
+  return solutionArray.join("")
 }
 
 export function returnPositiveOdd(n) {
-    let numOdds = 0
-    for (let i = 1; i < n; i++) {
-        if (i % 2 !== 0) {
-            numOdds++
-        }
+  let numOdds = 0
+  for (let i = 1; i < n; i++) {
+    if (i % 2 !== 0) {
+      numOdds++
     }
+  }
 
-    return numOdds
+  return numOdds
 }
 
-export function howMuchILoveYou (nbPetals) {
-    switch ((nbPetals - 1) % 6) {
-        case 1:
-            return "a little";
-        case 2:
-            return "a lot";
-        case 3:
-            return "passionately";
-        case 4:
-            return "madly";
-        case 5:
-            return "not at all";
-        default:
-            return "I love you";
-    }
+export function howMuchILoveYou(nbPetals) {
+  switch ((nbPetals - 1) % 6) {
+    case 1:
+      return "a little";
+    case 2:
+      return "a lot";
+    case 3:
+      return "passionately";
+    case 4:
+      return "madly";
+    case 5:
+      return "not at all";
+    default:
+      return "I love you";
+  }
 }
 
-export function fakeFilter (array, num) {
+export function fakeFilter(array, num) {
+  let newArray = []
 
-}
-
-export function boredom (staff){
-    let boredomScore = 0
-  
-    Object.values(staff).forEach(name => {
-       switch (name) {
-           case 'accounts':
-              boredomScore += 1;
-              break;
-           case 'finance':
-              boredomScore += 2;
-              break;
-           case 'canteen':
-              boredomScore += 10;
-              break;
-           case 'regulation':
-              boredomScore  += 3;
-              break;
-           case 'trading':
-              boredomScore += 6;
-              break;
-            case 'change':
-              boredomScore += 6;
-              break;
-            case 'IS':
-              boredomScore += 8;
-              break;
-            case 'retail':
-              boredomScore += 5;
-              break;
-            case 'cleaning':
-              boredomScore += 4;
-              break;
-            case 'pissing about':
-              boredomScore += 25;
-              break;
-       }
-    })
-    
-    if(boredomScore <= 80){
-      return 'kill me now'
-    } else if(boredomScore < 100){
-      return 'i can handle this'
+  array.forEach((x) => {
+    if (x === num) {
+      return
     } else {
-      return 'party time!!'
-}
-
-}
-
-export function lovefunc(flower1, flower2){
-    if(flower1 % 2 === 0 && flower2 % 2 != 0 || flower2 % 2 === 0 && flower1 % 2 != 0 ){
-      return true
-    } else {
-      return false
+      newArray.push(x)
     }
+  })
+
+  return newArray
+}
+
+export function boredom(staff) {
+  let boredomScore = 0
+
+  Object.values(staff).forEach(name => {
+    switch (name) {
+      case 'accounts':
+        boredomScore += 1;
+        break;
+      case 'finance':
+        boredomScore += 2;
+        break;
+      case 'canteen':
+        boredomScore += 10;
+        break;
+      case 'regulation':
+        boredomScore += 3;
+        break;
+      case 'trading':
+        boredomScore += 6;
+        break;
+      case 'change':
+        boredomScore += 6;
+        break;
+      case 'IS':
+        boredomScore += 8;
+        break;
+      case 'retail':
+        boredomScore += 5;
+        break;
+      case 'cleaning':
+        boredomScore += 4;
+        break;
+      case 'pissing about':
+        boredomScore += 25;
+        break;
+    }
+  })
+
+  if (boredomScore <= 80) {
+    return 'kill me now'
+  } else if (boredomScore < 100) {
+    return 'i can handle this'
+  } else {
+    return 'party time!!'
   }
 
-  export function find (a, e) {
-    let f = a.indexOf(e)
- 
-    return f === -1 ? "Not found" : f
+}
+
+export function lovefunc(flower1, flower2) {
+  if (flower1 % 2 === 0 && flower2 % 2 != 0 || flower2 % 2 === 0 && flower1 % 2 != 0) {
+    return true
+  } else {
+    return false
   }
+}
+
+export function find(a, e) {
+  let f = a.indexOf(e)
+
+  return f === -1 ? "Not found" : f
+}
 
 
-  export function removeExclamation (string) {
- 
-    let newString = string.replaceAll("!", "")
-    
-      newString += "!"
-    
-    return newString
-   
-  }
+export function removeExclamation(string) {
+
+  let newString = string.replaceAll("!", "")
+
+  newString += "!"
+
+  return newString
+
+}
--- cracking-coding-interview/code-wars/codeWars.test.js
+++ cracking-coding-interview/code-wars/codeWars.test.js
@@ -18,63 +18,73 @@ describe("code wars return number of positive odd", () => {
     })
 })
 
-describe("code wars petal challenge", ()=>{
-    it("determine which phrase the girls would say at the last petal for a flower of a given number of petals", ()=>{
+describe("code wars petal challenge", () => {
+    it("determine which phrase the girls would say at the last petal for a flower of a given number of petals", () => {
         expect(howMuchILoveYou(7)).toBe("I love you")
         expect(howMuchILoveYou(3)).toBe("a lot")
         expect(howMuchILoveYou(14)).toBe("a little")
         expect(howMuchILoveYou(9)).toBe("a lot")
+        expect(howMuchILoveYou(4)).toBe("passionately")
+        expect(howMuchILoveYou(5)).toBe("madly")
     })
 })
 
-//need dans help on this one
-describe("code wars filter challeng", ()=>{
-    it("create a filter function without using the filter function", ()=>{
-        expect(fakeFilter([10, 12, 14, 17, 15], 17)).toBe([10, 12, 14, 15])
+
+describe("code wars filter challeng", () => {
+    it("create a filter function without using the filter function", () => {
+        expect(fakeFilter([10, 12, 14, 17, 15], 17)).toEqual([10, 12, 14, 15])
+        expect(fakeFilter([10, 12, 14, 17, 15], 15)).toEqual([10, 12, 14, 17])
+        expect(fakeFilter([10, 12, 14, 17, 15], 11)).toEqual([10, 12, 14, 17, 15])
+        expect(fakeFilter([10, 10, 12, 14, 17, 15], 10)).toEqual([12, 14, 17, 15])
     })
 })
 
-// describe("code wars Boredom Score challenge", ()=>{
-//     it("takes in an object of staff members with names as keys and what they are doing as values and returens a boredom score for the office", ()=>{
-//         expect(boredom({tim: 'change', jim: 'accounts',
-//             randy: 'canteen', sandy: 'change', andy: 'change', katie: 'IS',
-//             laura: 'change', saajid: 'IS', alex: 'trading', john: 'accounts',
-//             mr: 'finance' })).toBe('kill me now')
-//         expect(boredom({ tim: 'IS', jim: 'finance',
-//             randy: 'pissing about', sandy: 'cleaning', andy: 'cleaning',
-//             katie: 'cleaning', laura: 'pissing about', saajid: 'regulation',
-//             alex: 'regulation', john: 'accounts', mr: 'canteen' }))
-//     .toBe('i can handle this')
-// })
-
-// describe("code wars love function challenge"), () => {
-//     it("two people pick flowers if one flower has an even number of petals and the other has an odd they are in love...return true", ()=>{
-//         expect(lovefunc(1,4)).toBe(true)
-//         expect(lovefunc(2,2)).toBe(false)
-//         expect(lovefunc(0,1)).toBe(true)
-//         expect(lovefunc(10, 7)).toBe(true)
-//     })
-// }
+describe("code wars Boredom Score challenge", () => {
+    it("takes in an object of staff members with names as keys and what they are doing as values and returens a boredom score for the office", () => {
+        expect(boredom({
+            tim: 'change', jim: 'accounts',
+            randy: 'canteen', sandy: 'change', andy: 'change', katie: 'IS',
+            laura: 'change', saajid: 'IS', alex: 'trading', john: 'accounts',
+            mr: 'finance'
+        })).toBe('kill me now')
+        expect(boredom({
+            tim: 'IS', jim: 'finance',
+            randy: 'pissing about', sandy: 'retail', andy: 'cleaning',
+            katie: 'cleaning', laura: 'pissing about', saajid: 'regulation',
+            alex: 'regulation', john: 'accounts', mr: 'canteen'
+        }))
+            .toBe('i can handle this')
+    })
+})
 
 describe("code wars love function challenge", () => {
     it("two people pick flowers if one flower has an even number of petals and the other has an odd they are in love...return true", () => {
-        expect(lovefunc(1,4)).toBe(true)
-        expect(lovefunc(2,2)).toBe(false)
-        expect(lovefunc(0,1)).toBe(true)
+        expect(lovefunc(1, 4)).toBe(true)
+        expect(lovefunc(2, 2)).toBe(false)
+        expect(lovefunc(0, 1)).toBe(true)
         expect(lovefunc(10, 7)).toBe(true)
     })
 })
 
-describe("code wars refactor find the index of an array", ()=>{
-    it("create a function that accepts an array and element and return the index of the element. less the 85 characters", ()=>{
-        expect(find([2,3,5,7,11], 5)).toBe(2)
-        expect(find([2,3,5,7,11], 11)).toBe(4)
-        expect(find([2,3,5,7,11], 6)).toBe("Not found")
+describe("code wars love function challenge", () => {
+    it("two people pick flowers if one flower has an even number of petals and the other has an odd they are in love...return true", () => {
+        expect(lovefunc(1, 4)).toBe(true)
+        expect(lovefunc(2, 2)).toBe(false)
+        expect(lovefunc(0, 1)).toBe(true)
+        expect(lovefunc(10, 7)).toBe(true)
     })
 })
 
-describe("code wars remove all exclamation points from a string except the one at the end", ()=>{
-    it("remvoes all exclamation points from a string except for the one at the end of the string" , ()=>{
+describe("code wars refactor find the index of an array", () => {
+    it("create a function that accepts an array and element and return the index of the element. less the 85 characters", () => {
+        expect(find([2, 3, 5, 7, 11], 5)).toBe(2)
+        expect(find([2, 3, 5, 7, 11], 11)).toBe(4)
+        expect(find([2, 3, 5, 7, 11], 6)).toBe("Not found")
+    })
+})
+
+describe("code wars remove all exclamation points from a string except the one at the end", () => {
+    it("remvoes all exclamation points from a string except for the one at the end of the string", () => {
         expect(removeExclamation("Hi!!!!")).toBe("Hi!")
         expect(removeExclamation("Hey! I love you!")).toBe("Hey I love you!")
         expect(removeExclamation("Clay rules!")).toBe("Clay rules!")
